f95e63a832d5f0ea7d5e66fd1dabae61edd8cf90
whitescent <Nthily@outlook.com>
2024-06-18 23:58:53 +0800

    build: add material3, upgrade compose version

--- modules/gallery/build.gradle.kts
+++ modules/gallery/build.gradle.kts
@@ -60,6 +60,7 @@ kotlin {
       implementation(compose.runtime)
       implementation(compose.foundation)
       implementation(compose.material)
+      implementation(compose.material3)
       implementation(compose.ui)
       implementation(compose.components.resources)
       implementation(compose.components.uiToolingPreview)
