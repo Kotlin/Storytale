71323d3d76cf01fb9dd43667b14777a9f112f321
WhiteScent <rouxicoc@outlook.com>
2024-07-07 20:20:28 +0800

    feat(gallery): remove StoryContentWrapper.kt, add border for Parameter Drawer

--- modules/gallery/src/commonMain/kotlin/StoryGallery.kt
+++ modules/gallery/src/commonMain/kotlin/StoryGallery.kt
@@ -1,10 +1,12 @@
 package org.jetbrains.compose.storytale.gallery
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -12,12 +14,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.storytale.Story
 import org.jetbrains.compose.storytale.gallery.compose.ScreenSize
 import org.jetbrains.compose.storytale.gallery.compose.isMobile
-import org.jetbrains.compose.storytale.gallery.story.StoryContentWrapper
 import org.jetbrains.compose.storytale.gallery.story.StoryNavigationBar
 import org.jetbrains.compose.storytale.gallery.story.StoryParameterDrawer
 import org.jetbrains.compose.storytale.gallery.ui.component.HorizontalSplitPane
@@ -46,10 +48,20 @@ fun StoryGallery(stories: List<Story>) {
             contentAlignment = Alignment.Center
           ) {
             with(activeStory) {
-              StoryContentWrapper(name, content)
+              content()
             }
           }
-          StoryParameterDrawer(activeStory)
+          StoryParameterDrawer(
+            activeStory = activeStory,
+            modifier = Modifier
+              .border(
+                width = 1.dp,
+                color = Color.Black.copy(.11f),
+                shape = RoundedCornerShape(topStart = 36.dp, bottomStart = 36.dp)
+              )
+              .clip(RoundedCornerShape(topStart = 36.dp, bottomStart = 36.dp))
+              .background(Color.White)
+          )
         }
       }
     )
--- modules/gallery/src/commonMain/kotlin/story/StoryParameterDrawer.kt
+++ modules/gallery/src/commonMain/kotlin/story/StoryParameterDrawer.kt
@@ -1,6 +1,5 @@
 package org.jetbrains.compose.storytale.gallery.story
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,13 +8,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,8 +32,6 @@ fun StoryParameterDrawer(
 ) = Box(
   modifier = modifier.fillMaxHeight()
     .widthIn(max = 280.dp)
-    .clip(RoundedCornerShape(topStart = 36.dp, bottomStart = 36.dp))
-    .background(Color.White)
 ) {
   Column(Modifier.padding(horizontal = 24.dp, vertical = 28.dp)) {
     CenterRow {
