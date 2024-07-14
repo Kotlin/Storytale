cb8575c0ab66fe57558e31937b36b80cae7b13ed
WhiteScent <rouxicoc@outlook.com>
2024-07-14 15:01:17 +0800

    chore: optimize the implementation of Horizontal

--- modules/gallery/src/commonMain/kotlin/ui/component/HorizontalSplitPane.kt
+++ modules/gallery/src/commonMain/kotlin/ui/component/HorizontalSplitPane.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.compose.storytale.gallery.ui.component
 
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -13,88 +15,86 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.SubcomposeLayout
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.coerceIn
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.fastForEach
-import androidx.compose.ui.util.fastMap
-
-enum class HorizontalSplitPaneSlot {
-  Left,
-  Divider,
-  Right
-}
+import org.jetbrains.compose.storytale.gallery.compose.currentDensity
 
 @Composable
 fun HorizontalSplitPane(
   modifier: Modifier = Modifier,
-  minimumWidth: Dp = 250.dp,
-  left: @Composable () -> Unit,
-  right: @Composable () -> Unit
+  content: @Composable () -> Unit
 ) {
-  var dividerOffset by remember { mutableStateOf(minimumWidth) }
-  SubcomposeLayout(
+  var dividerOffset by remember { mutableStateOf(0.dp) }
+  Layout(
     modifier = modifier,
-  ) { constraints ->
-    val layoutWidth = constraints.maxWidth
-    val leftArea = subcompose(
-      slotId = HorizontalSplitPaneSlot.Left,
-      content = { left() }
-    ).fastMap {
-      it.measure(
-        constraints.copy(
-          maxWidth = dividerOffset.coerceIn(
-            minimumValue = 0.dp,
-            maximumValue = layoutWidth.toDp() - minimumWidth
-          ).roundToPx()
-        )
-      )
-    }
-
-    val divider = subcompose(
-      slotId = HorizontalSplitPaneSlot.Divider,
-      content = {
-        Divider(
-          modifier = Modifier.fillMaxHeight()
-            .clip(CircleShape)
-            .width(6.dp)
-            .pointerInput(layoutWidth) {
-              detectDragGestures { _, dragAmount ->
-                dividerOffset = (dividerOffset + dragAmount.x.toDp())
-                  .coerceIn(
-                    minimumValue = 0.dp,
-                    maximumValue = layoutWidth.toDp() - minimumWidth
-                  )
-              }
-            },
-          color = Color(0xFFDADADA)
-        )
+    content = {
+      content()
+      VerticalSplitDivider {
+        dividerOffset += it
       }
-    ).fastMap { it.measure(constraints) }
-
-    val rightArea = subcompose(
-      slotId = HorizontalSplitPaneSlot.Right,
-      content = right
-    ).fastMap {
-      it.measure(
-        constraints.copy(
-          maxWidth = (layoutWidth - leftArea.first().width - divider.first().width)
-            .coerceAtLeast(0)
-        )
-      )
     }
+  ) { measurables, constraints ->
+    require(measurables.size == 3) {
+      "HorizontalSplitPane only supports plays with two @Composable content, " +
+        "the @Composable functions to the left and right of the splitter line."
+    }
+    val dividerWidth = measurables[2].maxIntrinsicWidth(constraints.maxHeight)
+    val firstPlaceable = measurables[0].measure(
+      constraints.copy(
+        maxWidth = (250.dp + dividerOffset).coerceIn(
+          minimumValue = 0.dp,
+          maximumValue = constraints.maxWidth.toDp()
+        ).roundToPx()
+      )
+    )
+    val secondWidth = (constraints.maxWidth - firstPlaceable.width - dividerWidth)
+      .coerceIn(minimumValue = 0, maximumValue = constraints.maxWidth)
+    val secondPlaceable = measurables[1].measure(
+      Constraints(
+        minWidth = secondWidth,
+        maxWidth = secondWidth,
+        minHeight = constraints.maxHeight,
+        maxHeight = constraints.maxHeight
+      )
+    )
+    val splitterPlaceable = measurables[2].measure(constraints)
     layout(constraints.maxWidth, constraints.maxHeight) {
-      leftArea.fastForEach {
-        it.place(0, 0)
-      }
-      divider.fastForEach {
-        it.place(leftArea.first().width, 0)
-      }
-      rightArea.fastForEach {
-        it.place(divider.first().width + leftArea.first().width, 0)
-      }
+      firstPlaceable.place(0, 0)
+      secondPlaceable.place(
+        x = firstPlaceable.width + dividerWidth,
+        y = 0
+      )
+      splitterPlaceable.place(firstPlaceable.width, 0)
     }
   }
+}
+
+@Composable
+private fun VerticalSplitDivider(
+  modifier: Modifier = Modifier,
+  shape: Shape = CircleShape,
+  color: Color = Color(0xFFDADADA),
+  width: Dp = 4.dp,
+  onDrag: (Dp) -> Unit
+) {
+  val density = currentDensity
+  Divider(
+    modifier = modifier.fillMaxHeight()
+      .clip(shape)
+      .width(width)
+      .draggable(
+        state = rememberDraggableState { offset ->
+          with(density) {
+            onDrag(offset.toDp())
+          }
+        },
+        orientation = Orientation.Horizontal,
+        startDragImmediately = true
+      ),
+    color = color
+  )
 }
