d9fcd256d8bd6223515c385bdac4c88729e9e2d7
WhiteScent <rouxicoc@outlook.com>
2024-08-21 23:51:53 +0800

    fix: BooleanParameterField layout

--- modules/gallery/src/commonMain/kotlin/story/parameters/BooleanParameterField.kt
+++ modules/gallery/src/commonMain/kotlin/story/parameters/BooleanParameterField.kt
@@ -2,7 +2,10 @@ package org.jetbrains.compose.storytale.gallery.story.parameters
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -19,22 +22,22 @@ fun BooleanParameterField(
   modifier: Modifier = Modifier,
 ) = Column(modifier = modifier) {
   var checked by state
-
   CenterRow {
-    Text(
-      text = parameterName,
-      style = currentTypography.parameterText,
-      maxLines = 1,
-      overflow = TextOverflow.Ellipsis
+    CenterRow(Modifier.weight(1f)) {
+      Text(
+        text = parameterName,
+        style = currentTypography.parameterText,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis
+      )
+      Gap(6.dp)
+      ParameterLabel("Boolean")
+    }
+    SwitchButton(
+      checked = checked,
+      onValueChange = { checked = it }
     )
-    Gap(6.dp)
-    ParameterLabel("Boolean")
   }
-  Gap(12.dp)
-  SwitchButton(
-    checked = checked,
-    onValueChange = { checked = it }
-  )
   if (description.isNotEmpty()) {
     Gap(12.dp)
     Text(
