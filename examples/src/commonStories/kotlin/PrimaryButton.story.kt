7c5fd5db2dfffd6b5952c34f10d36c1f7a13548f
Artem Kobzar <artem.kobzar@jetbrains.com>
2024-09-06 17:34:50 +0200

    Add GitHub pages for the example stories

--- examples/src/commonStories/kotlin/PrimaryButton.story.kt
+++ examples/src/commonStories/kotlin/PrimaryButton.story.kt
@@ -3,18 +3,18 @@ import org.jetbrains.compose.storytale.story
 
 val PrimaryButton by story {
   val enabled by parameter(true)
-  val text by parameter("Click Me \uD83C\uDF1F")
+  val text by parameter("Click Me")
 
   PrimaryButton(text = text, enabled = enabled)
 }
 
 val `PrimaryButton Default State` by story {
-  PrimaryButton("Click Me \uD83C\uDF1F")
+  PrimaryButton("Click Me")
 }
 
 val `PrimaryButton Disabled` by story {
   PrimaryButton(
-    text = "Click Me \uD83C\uDF1F",
+    text = "Click Me",
     enabled = false
   )
 }
