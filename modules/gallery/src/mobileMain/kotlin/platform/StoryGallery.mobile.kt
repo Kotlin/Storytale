55b727014507f6e01d7068f0f6bdf27e20ea81d9
WhiteScent <rouxicoc@outlook.com>
2024-10-30 00:00:33 +0800

    feat(gallery): use navigation type-safety

--- modules/gallery/src/mobileMain/kotlin/platform/StoryGallery.mobile.kt
+++ modules/gallery/src/mobileMain/kotlin/platform/StoryGallery.mobile.kt
@@ -8,11 +8,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.dropUnlessResumed
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import org.jetbrains.compose.storytale.Story
 import org.jetbrains.compose.storytale.gallery.navigation.Route
 import org.jetbrains.compose.storytale.gallery.story.StoryNavigationBar
@@ -25,68 +24,27 @@ actual fun StoryGallery(
   modifier: Modifier
 ) {
   val navController = rememberNavController()
-// for type-safety navigation
-//  NavHost(
-//    navController = navController,
-//    startDestination = Route.MobileHome,
-//    modifier = modifier,
-//    enterTransition = { slideIntoContainer(Start, tween(defaultSlideAnimationTween)) },
-//    exitTransition = { slideOutOfContainer(End, tween(defaultSlideAnimationTween)) }
-//  ) {
-//    composable<Route.MobileHome>(
-//      enterTransition = { slideIntoContainer(End, tween(defaultSlideAnimationTween)) },
-//      exitTransition = { slideOutOfContainer(Start, tween(defaultSlideAnimationTween)) }
-//    ) {
-//      StoryNavigationBar(
-//        stories = stories,
-//        onSelectStory = { storyId ->
-//          navController.navigate(Route.MobileGallery(storyId))
-//        },
-//        activeStoryId = -1
-//      )
-//    }
-//    composable<Route.MobileGallery> { backStackEntry ->
-//      val storyId = backStackEntry.toRoute<Route.MobileGallery>().storyId
-//      Column {
-//        MobileGallery(
-//          story = stories.find { it.id == storyId },
-//          back = dropUnlessResumed {
-//            navController.popBackStack()
-//          }
-//        )
-//      }
-//    }
-//  }
-
   NavHost(
     navController = navController,
-    startDestination = Route.MobileHome.route,
+    startDestination = Route.MobileHome,
     modifier = modifier,
     enterTransition = { slideIntoContainer(Start, tween(defaultSlideAnimationTween)) },
     exitTransition = { slideOutOfContainer(End, tween(defaultSlideAnimationTween)) }
   ) {
-    composable(
-      route = Route.MobileHome.route,
+    composable<Route.MobileHome>(
       enterTransition = { slideIntoContainer(End, tween(defaultSlideAnimationTween)) },
       exitTransition = { slideOutOfContainer(Start, tween(defaultSlideAnimationTween)) }
     ) {
       StoryNavigationBar(
         stories = stories,
         onSelectStory = { storyId ->
-          navController.navigate(
-            route = Route.MobileGallery.route.replace("{storyId}", storyId.toString())
-          )
+          navController.navigate(Route.MobileGallery(storyId))
         },
         activeStoryId = -1
       )
     }
-    composable(
-      route = Route.MobileGallery.route,
-      arguments = listOf(
-        navArgument("storyId") { type = NavType.IntType }
-      )
-    ) { backStackEntry ->
-      val storyId = backStackEntry.arguments?.getInt("storyId")
+    composable<Route.MobileGallery> { backStackEntry ->
+      val storyId = backStackEntry.toRoute<Route.MobileGallery>().storyId
       Column {
         MobileGallery(
           story = stories.find { it.id == storyId },
