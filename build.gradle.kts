9fa16175f60a0481ce8cf657889f1ef7409105bd
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2024-12-12 11:58:53 +0100

    Add ComposeRepo maven repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,24 @@ plugins {
   alias(libs.plugins.kotlinMultiplatform) apply false
   alias(libs.plugins.compose.compiler) apply false
 }
+
+subprojects {
+  plugins.withId("maven-publish") {
+    configureIfExists<PublishingExtension> {
+      repositories {
+        maven {
+          name = "ComposeRepo"
+          setUrl(System.getenv("COMPOSE_REPO_URL"))
+          credentials {
+            username = System.getenv("COMPOSE_REPO_USERNAME")
+            password = System.getenv("COMPOSE_REPO_KEY")
+          }
+        }
+      }
+    }
+  }
+}
+
+inline fun <reified T> Project.configureIfExists(fn: T.() -> Unit) {
+  extensions.findByType(T::class.java)?.fn()
+}
