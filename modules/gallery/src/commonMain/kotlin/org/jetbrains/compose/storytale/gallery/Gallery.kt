0948e29c0013258c6f077cfd7e97d86e1b9a6dfe
whitescent <Nthily@outlook.com>
2025-02-06 00:18:43 +0800

    fix: remove @Preview from Gallery

--- modules/gallery/src/commonMain/kotlin/org/jetbrains/compose/storytale/gallery/Gallery.kt
+++ modules/gallery/src/commonMain/kotlin/org/jetbrains/compose/storytale/gallery/Gallery.kt
@@ -4,11 +4,9 @@ import androidx.compose.runtime.Composable
 import org.jetbrains.compose.storytale.gallery.platform.StoryGallery
 import org.jetbrains.compose.storytale.gallery.ui.theme.StoryGalleryTheme
 import org.jetbrains.compose.storytale.storiesStorage
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-@Preview
-private fun Gallery() {
+fun Gallery() {
   StoryGalleryTheme {
     StoryGallery(storiesStorage)
   }
