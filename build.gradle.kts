d388644f71bc9b13545e20a9ee266c221223f6fb
whitescent <Nthily@outlook.com>
2025-02-05 23:42:09 +0800

    feat: introduce spotless + ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 plugins {
   // this is necessary to avoid the plugins to be loaded multiple times
   // in each subproject's classloader
@@ -8,6 +10,7 @@ plugins {
   alias(libs.plugins.compose.compiler) apply false
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.dokka) apply false
+  alias(libs.plugins.spotless) apply false
 }
 
 subprojects {
@@ -28,8 +31,27 @@ subprojects {
       }
     }
   }
+  plugins.apply(rootProject.libs.plugins.spotless.get().pluginId)
+  extensions.configure<SpotlessExtension> {
+    kotlin {
+      target("src/**/*.kt")
+      targetExclude("src/test/resources/**")
+      ktlint(libs.ktlint.get().version)
+        .editorConfigOverride(
+          mapOf(
+            "ktlint_compose_modifier-missing-check" to "disabled",
+            "ktlint_compose_compositionlocal-allowlist" to "disabled",
+          ),
+        )
+        .customRuleSets(listOf(libs.composeRules.get().toString()))
+    }
+    kotlinGradle {
+      ktlint(libs.ktlint.get().version)
+    }
+  }
 }
 
 inline fun <reified T> Project.configureIfExists(fn: T.() -> Unit) {
   extensions.findByType(T::class.java)?.fn()
 }
+
