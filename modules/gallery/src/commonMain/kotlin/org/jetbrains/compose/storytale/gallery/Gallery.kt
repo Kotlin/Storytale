21791f1fd8cc4ef14d90397b40604b1d44e2a24e
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2025-04-03 11:28:00 +0200

    Use new StorytaleGalleryApp in Gallery Composable

--- modules/gallery/src/commonMain/kotlin/org/jetbrains/compose/storytale/gallery/Gallery.kt
+++ modules/gallery/src/commonMain/kotlin/org/jetbrains/compose/storytale/gallery/Gallery.kt
@@ -1,13 +1,12 @@
 package org.jetbrains.compose.storytale.gallery
 
 import androidx.compose.runtime.Composable
+import org.jetbrains.compose.storytale.gallery.material3.StorytaleGalleryApp
 import org.jetbrains.compose.storytale.gallery.platform.StoryGallery
 import org.jetbrains.compose.storytale.gallery.ui.theme.StoryGalleryTheme
 import org.jetbrains.compose.storytale.storiesStorage
 
 @Composable
 fun Gallery() {
-    StoryGalleryTheme {
-        StoryGallery(storiesStorage)
-    }
+    StorytaleGalleryApp()
 }
