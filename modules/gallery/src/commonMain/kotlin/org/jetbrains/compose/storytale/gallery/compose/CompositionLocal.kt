8a3a3103480ce26a0d2fec1818c24b7c0c110d5d
Cong Vu Chi <129714106+congvc-dev@users.noreply.github.com>
2025-05-02 22:08:16 +0700

    fix: param settings FAB obstructs StoryContent (#68)
    * bump agpt 8.9.2
    
    * fix: param settings FAB obstructs StoryContent
    
    * fix spotless
    
    * bump gradle 8.11.1

--- modules/gallery/src/commonMain/kotlin/org/jetbrains/compose/storytale/gallery/compose/CompositionLocal.kt
+++ modules/gallery/src/commonMain/kotlin/org/jetbrains/compose/storytale/gallery/compose/CompositionLocal.kt
@@ -1,5 +1,9 @@
 package org.jetbrains.compose.storytale.gallery.compose
 
+import androidx.compose.runtime.staticCompositionLocalOf
+
 inline fun <reified T> noCompositionLocalProvided(): T {
     error("CompositionLocal ${T::class.simpleName} not present")
 }
+
+val LocalIsEmbeddedView = staticCompositionLocalOf { false }
