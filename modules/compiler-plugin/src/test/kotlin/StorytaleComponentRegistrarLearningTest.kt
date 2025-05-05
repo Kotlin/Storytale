991f58ef0c737e6b166e55d3258161b60cb749a7
Vu Chi Cong <nogogold1998@live.com>
2025-05-05 10:11:15 +0700

    refactoring MentionAllStoriesGettersInsideMainFunctionLoweringTest.kt

--- modules/compiler-plugin/src/test/kotlin/StorytaleComponentRegistrarLearningTest.kt
+++ modules/compiler-plugin/src/test/kotlin/StorytaleComponentRegistrarLearningTest.kt
@@ -1,31 +1,60 @@
+import androidx.compose.compiler.plugins.kotlin.ComposePluginRegistrar
 import com.tschuchort.compiletesting.KotlinCompilation
-import kotlin.test.Test
+import com.tschuchort.compiletesting.SourceFile
 import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.compose.plugin.storytale.compiler.StorytaleComponentRegistrar
 import org.jetbrains.compose.storytale.storiesStorage
-import util.storytaleTest
+import org.junit.Test
 
-class MentionAllStoriesGettersInsideMainFunctionLoweringTest {
+/**
+ * This test is a learning test for new comers. Please don't over engineer it
+ */
+class StorytaleComponentRegistrarLearningTest {
+
     @Test
     fun `all stories are registered after compilation and main invocation`() {
-        val result = storytaleTest {
-            "Group1.story.kt" hasContent """
+        // reset to initialize state
+        storiesStorage.clear()
+
+        val group1Kt = SourceFile.kotlin(
+            "Group1.story.kt",
+            """
                 package storytale.gallery.demo
                 import org.jetbrains.compose.storytale.story
 
                 val Story1 by story { }
                 val Story2 by story { }
+    """,
+        )
+        val group2Kt = SourceFile.kotlin(
+            "Group2.story.kt",
             """
-            "Group2.story.kt" hasContent """
                 package storytale.gallery.demo
                 import org.jetbrains.compose.storytale.story
 
                 val Story3 by story { }
+    """,
+        )
+        val mainKt = SourceFile.kotlin(
+            "Main.kt",
             """
-            "Main.kt" hasContent """
                 package org.jetbrains.compose.storytale.generated
                 fun MainViewController() { }
-            """
+    """,
+        )
+
+        val result = KotlinCompilation().apply {
+            sources = listOf(group1Kt, group2Kt, mainKt)
+
+            compilerPluginRegistrars =
+                listOf(StorytaleComponentRegistrar(), ComposePluginRegistrar())
+            // magic
+            inheritClassPath = true
+            messageOutputStream = System.out // see diagnostics in real time
+            jvmTarget = "21"
+            verbose = false
         }
+            .compile()
 
         assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
 
@@ -43,4 +72,5 @@ class MentionAllStoriesGettersInsideMainFunctionLoweringTest {
         assertThat(storiesStorage.map { it.group }.toSet())
             .containsExactlyInAnyOrder("Group1", "Group2")
     }
+
 }
