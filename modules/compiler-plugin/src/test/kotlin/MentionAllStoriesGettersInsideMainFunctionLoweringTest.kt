6628f542c10909c6483e115fb4090a05a5cfaf5e
Vu Chi Cong <nogogold1998@live.com>
2025-05-05 09:55:52 +0700

    add StorytaleTest.kt

--- modules/compiler-plugin/src/test/kotlin/MentionAllStoriesGettersInsideMainFunctionLoweringTest.kt
+++ modules/compiler-plugin/src/test/kotlin/MentionAllStoriesGettersInsideMainFunctionLoweringTest.kt
@@ -5,10 +5,17 @@ import kotlin.test.Test
 import org.assertj.core.api.Assertions.assertThat
 import org.jetbrains.compose.plugin.storytale.compiler.StorytaleComponentRegistrar
 import org.jetbrains.compose.storytale.storiesStorage
+import util.storytaleTest
 
 class MentionAllStoriesGettersInsideMainFunctionLoweringTest {
+    /**
+     * This test is a learning test for new comers. Please don't over engineer it
+     */
     @Test
-    fun testAllStoriesAreRegisteredAfterCompilationAndMainInvocation() {
+    fun `all stories are registered after compilation and main invocation`() {
+        // reset to initialize state
+        storiesStorage.clear()
+
         val group1Kt = SourceFile.kotlin(
             "Group1.story.kt",
             """
@@ -41,12 +48,52 @@ class MentionAllStoriesGettersInsideMainFunctionLoweringTest {
 
             compilerPluginRegistrars =
                 listOf(StorytaleComponentRegistrar(), ComposePluginRegistrar())
+            // magic
             inheritClassPath = true
             messageOutputStream = System.out // see diagnostics in real time
             jvmTarget = "21"
             verbose = false
         }
             .compile()
+
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+
+        val clazz = result.classLoader.loadClass("org.jetbrains.compose.storytale.generated.MainKt")
+
+        assertThat(clazz).isNotNull
+        assertThat(clazz).hasDeclaredMethods("MainViewController")
+
+        clazz.getMethod("MainViewController").invoke(null)
+
+        // CRAZY!!!
+        assertThat(storiesStorage).hasSize(3)
+        assertThat(storiesStorage.map { it.name })
+            .containsExactlyInAnyOrder("Story1", "Story2", "Story3")
+        assertThat(storiesStorage.map { it.group }.toSet())
+            .containsExactlyInAnyOrder("Group1", "Group2")
+    }
+
+    @Test
+    fun `same as above test but over engineered`() {
+        val result = storytaleTest {
+            "Group1.story.kt" hasContent """
+                package storytale.gallery.demo
+                import org.jetbrains.compose.storytale.story
+
+                val Story1 by story { }
+                val Story2 by story { }
+            """
+            "Group2.story.kt" hasContent """
+                package storytale.gallery.demo
+                import org.jetbrains.compose.storytale.story
+
+                val Story3 by story { }
+            """
+            "Main.kt" hasContent """
+                package org.jetbrains.compose.storytale.generated
+                fun MainViewController() { }
+            """
+        }
 
         assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
 
---
+++ modules/compiler-plugin/src/test/kotlin/util/StorytaleTest.kt
@@ -0,0 +1,51 @@
+package util
+
+import androidx.compose.compiler.plugins.kotlin.ComposePluginRegistrar
+import com.tschuchort.compiletesting.JvmCompilationResult
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import org.jetbrains.compose.plugin.storytale.compiler.StorytaleComponentRegistrar
+import org.jetbrains.compose.storytale.storiesStorage
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+import org.intellij.lang.annotations.Language
+
+@OptIn(ExperimentalContracts::class)
+fun storytaleTest(
+    compilationBuilder: KotlinCompilation.() -> Unit = {},
+    testSourceBuilder: StorytaleTestSourceScope.() -> Unit,
+): JvmCompilationResult {
+    contract {
+        callsInPlace(compilationBuilder, InvocationKind.EXACTLY_ONCE)
+        callsInPlace(testSourceBuilder, InvocationKind.EXACTLY_ONCE)
+    }
+    storiesStorage.clear()
+
+    val testSourceScope = object : StorytaleTestSourceScope {
+        val sources = mutableListOf<SourceFile>()
+        override fun String.hasContent(content: String) {
+            sources.add(SourceFile.Companion.kotlin(this, content))
+        }
+    }
+
+    val compilationSetup = KotlinCompilation().apply {
+        compilerPluginRegistrars =
+            listOf(StorytaleComponentRegistrar(), ComposePluginRegistrar())
+        inheritClassPath = true
+        messageOutputStream = System.out // see diagnostics in real time
+        jvmTarget = "21"
+        verbose = false
+    }
+
+    compilationSetup.compilationBuilder()
+    testSourceScope.testSourceBuilder()
+
+    compilationSetup.sources = testSourceScope.sources
+
+    return compilationSetup.compile()
+}
+
+interface StorytaleTestSourceScope {
+    infix fun String.hasContent(@Language("kotlin") content: String)
+}
