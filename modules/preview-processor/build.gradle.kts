469d9f2a69f5d34c6a4a106e69fddb6e015799ec
Vu Chi Cong <nogogold1998@live.com>
2025-05-09 00:06:41 +0700

    Add PreviewProcessor and first good test

--- modules/preview-processor/build.gradle.kts
+++ modules/preview-processor/build.gradle.kts
@@ -1,5 +1,12 @@
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.compose.ComposePlugin.CommonComponentsDependencies
+import org.jetbrains.compose.ComposePlugin.CommonComponentsDependencies.uiToolingPreview
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
@@ -12,10 +19,18 @@ java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
 }
-dependencies{
-    implementation("com.google.devtools.ksp:symbol-processing-api:2.1.20-2.0.1")
+
+dependencies {
+    compileOnly(compose.runtime)
+    implementation(libs.ksp.api)
+    testImplementation(compose.components.uiToolingPreview)
+    testImplementation(compose.runtime)
     testImplementation(kotlin("test"))
     testImplementation(libs.assertj.core)
     testImplementation(libs.junit)
-    testImplementation(libs.kotlinCompileTesting.core)
+    testImplementation(libs.kotlinCompileTesting.ksp)
+}
+
+tasks.withType<KotlinCompilationTask<*>>().configureEach {
+    compilerOptions.optIn.add("org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi")
 }
---
+++ modules/preview-processor/src/main/kotlin/PreviewProcessor.kt
@@ -0,0 +1,25 @@
+import com.google.devtools.ksp.processing.CodeGenerator
+import com.google.devtools.ksp.processing.KSPLogger
+import com.google.devtools.ksp.processing.Resolver
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.processing.SymbolProcessorProvider
+import com.google.devtools.ksp.symbol.KSAnnotated
+
+class PreviewProcessor(
+    private val logger: KSPLogger,
+    private val codeGenerator: CodeGenerator,
+) : SymbolProcessor {
+    override fun process(resolver: Resolver): List<KSAnnotated> {
+        return emptyList()
+    }
+
+    class Provider : SymbolProcessorProvider {
+        override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
+            return PreviewProcessor(
+                environment.logger,
+                environment.codeGenerator,
+            )
+        }
+    }
+}
