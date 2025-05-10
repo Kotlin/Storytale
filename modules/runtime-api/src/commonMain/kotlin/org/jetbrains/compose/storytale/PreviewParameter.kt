fba031be71544954de1a8eff173dbb9eac80f92b
Vu Chi Cong <nogogold1998@live.com>
2025-05-10 23:42:51 +0700

    Add previewParameter functions for non Story scopes

---
+++ modules/runtime-api/src/commonMain/kotlin/org/jetbrains/compose/storytale/PreviewParameter.kt
@@ -0,0 +1,22 @@
+package org.jetbrains.compose.storytale
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
+
+@Composable
+inline fun <reified T> previewParameter(defaultValue: T) =
+    LocalStory.current.parameter(defaultValue)
+
+@Composable
+inline fun <reified T> previewParameter(
+    values: List<T>,
+    defaultValueIndex: Int = 0,
+    label: String? = null,
+) = LocalStory.current.parameter(values, defaultValueIndex, label)
+
+@Composable
+inline fun <reified T : Enum<T>> previewParameter(defaultValue: T, label: String? = null) =
+    LocalStory.current.parameter(defaultValue, label)
+
+@PublishedApi
+internal val LocalStory = staticCompositionLocalOf { Story(-1, "DefaultPreviewStory", "", "", {}) }
