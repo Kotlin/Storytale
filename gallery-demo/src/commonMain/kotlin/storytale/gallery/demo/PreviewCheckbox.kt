652d2f41c862587ff3355c7e73b81d2d6d425998
Vu Chi Cong <nogogold1998@live.com>
2025-05-11 21:56:49 +0700

    Add @Preview composables samples in :gallery-demo

---
+++ gallery-demo/src/commonMain/kotlin/storytale/gallery/demo/PreviewCheckbox.kt
@@ -0,0 +1,27 @@
+package storytale.gallery.demo
+
+import androidx.compose.material3.TriStateCheckbox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.state.ToggleableState
+import androidx.compose.ui.state.ToggleableState.Indeterminate
+import androidx.compose.ui.state.ToggleableState.Off
+import androidx.compose.ui.state.ToggleableState.On
+import org.jetbrains.compose.storytale.previewParameter
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun PreviewCheckbox() {
+    var state by previewParameter(ToggleableState.entries)
+
+    TriStateCheckbox(
+        state = state,
+        onClick = {
+            state = when (state) {
+                On -> Indeterminate
+                Off -> On
+                Indeterminate -> Off
+            }
+        },
+    )
+}
