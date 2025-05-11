b84713dbe4d4cf4a6341436c3a1e7d033605865a
Vu Chi Cong <nogogold1998@live.com>
2025-05-11 21:56:49 +0700

    Add @Preview composables samples in :gallery-demo

--- gallery-demo/src/desktopMain/kotlin/storytale/gallery/demo/PreviewButton.kt
+++ gallery-demo/src/desktopMain/kotlin/storytale/gallery/demo/PreviewButton.kt
@@ -7,8 +7,13 @@ import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.storytale.previewParameter
@@ -23,5 +28,24 @@ fun PreviewExtendedFAB() {
         Icon(imageVector = Icons.Default.AddCircle, contentDescription = null)
         Spacer(Modifier.padding(4.dp))
         Text("Extended")
+    }
+}
+
+@androidx.compose.desktop.ui.tooling.preview.Preview
+@Composable
+@Suppress("ktlint")
+fun PreviewSegmentedButton() {
+    val selectedIndex = remember { mutableIntStateOf(0) }
+
+    SingleChoiceSegmentedButtonRow {
+        repeat(3) { index ->
+            SegmentedButton(
+                selected = index == selectedIndex.value,
+                onClick = { selectedIndex.value = index },
+                shape = SegmentedButtonDefaults.itemShape(index, 3),
+            ) {
+                Text("Button $index", modifier = Modifier.padding(4.dp))
+            }
+        }
     }
 }
