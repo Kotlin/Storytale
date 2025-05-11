885c84e222807ccc5c920c215adf58d33e3729d2
Vu Chi Cong <nogogold1998@live.com>
2025-05-11 13:02:08 +0700

    move PreviewProcessorTest to :modules:preview-processor-test

--- modules/preview-processor-test/src/jvmTest/kotlin/PreviewProcessorTest.kt
+++ modules/preview-processor-test/src/jvmTest/kotlin/PreviewProcessorTest.kt
@@ -7,13 +7,13 @@ import com.tschuchort.compiletesting.symbolProcessorProviders
 import com.tschuchort.compiletesting.useKsp2
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.util.Files
-import org.intellij.lang.annotations.Language
 import org.jetbrains.kotlin.utils.fileUtils.descendantRelativeTo
 import org.junit.Test
+import util.hasContent
 
 class PreviewProcessorTest {
     @Test
-    fun `generates story for single preview function`() {
+    fun `generates story for Jetbrains Compose Preview function`() {
         val group1Kt = SourceFile.kotlin(
             "KmpButton.kt",
             """
@@ -84,9 +84,64 @@ class PreviewProcessorTest {
                 """.trimMargin(),
             )
     }
-}
 
-@Suppress("NOTHING_TO_INLINE")
-inline infix fun String.hasContent(@Language("kotlin") content: String): Pair<String, String> {
-    return this to content
+    @Test
+    fun `generates story for Androidx Desktop Preview function`() {
+        val sourceFile = SourceFile.kotlin(
+            "DesktopButton.kt",
+            """
+                package storytale.gallery.demo
+
+                @androidx.compose.runtime.Composable
+                fun DesktopButton() { }
+
+                @androidx.compose.desktop.ui.tooling.preview.Preview
+                @androidx.compose.runtime.Composable
+                fun PreviewDesktopButton() {
+                    DesktopButton()
+                }
+            """,
+        )
+
+        val compilation = KotlinCompilation().apply {
+            sources = listOf(sourceFile)
+
+            compilerPluginRegistrars = listOf(ComposePluginRegistrar())
+            useKsp2()
+            symbolProcessorProviders.add(PreviewProcessor.Provider())
+
+            // magic
+            inheritClassPath = true
+            messageOutputStream = System.out // see diagnostics in real time
+            jvmTarget = "21"
+            verbose = false
+        }
+        val result = compilation
+            .compile()
+
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+
+        assertThat(result.sourcesGeneratedBySymbolProcessor.toList()).hasSize(1)
+
+        assertThat(
+            result.sourcesGeneratedBySymbolProcessor.toList().map {
+                it.descendantRelativeTo(compilation.kspSourcesDir).path to
+                    Files.contentOf(it, Charsets.UTF_8).trim()
+            },
+        )
+            .containsExactlyInAnyOrder(
+                "kotlin/org/jetbrains/compose/storytale/generated/Previews.story.kt" hasContent """
+                |package org.jetbrains.compose.storytale.generated
+                |
+                |import org.jetbrains.compose.storytale.Story
+                |import org.jetbrains.compose.storytale.story
+                |import storytale.gallery.demo.PreviewDesktopButton
+                |
+                |public val DesktopButton: Story by story {
+                |    PreviewDesktopButton()
+                |}
+                """.trimMargin(),
+            )
+    }
+
 }
