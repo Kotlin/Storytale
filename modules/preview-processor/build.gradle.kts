e13d1fb8683ae83af17bab72faee4a07b9b4ef81
Vu Chi Cong <nogogold1998@live.com>
2025-05-17 22:07:31 +0700

    Add PublicPreviewComposableRegistrar

--- modules/preview-processor/build.gradle.kts
+++ modules/preview-processor/build.gradle.kts
@@ -16,5 +16,6 @@ java {
 dependencies {
     implementation(libs.kotlin.poet)
     implementation(libs.ksp.api)
+    implementation(kotlin("compiler-embeddable"))
     implementation(project(":modules:gradle-plugin"))
 }
