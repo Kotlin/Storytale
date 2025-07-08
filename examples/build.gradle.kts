e2fde2efb669123bff2e7e5de5913bb15d281090
WhiteScent <rouxicoc@outlook.com>
2025-07-08 18:24:56 +0800

    feat: add gradle task to support gallery hot reload

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.hot.reload)
 }
 
 kotlin {
